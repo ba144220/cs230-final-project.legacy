55ede1583b0151d99986a49915879b35cedb16c8
ba144220 <b08901097@ntu.edu.tw>
2024-10-16 15:22:26 +0800

    feat: evaluation no trainer example setup

---
+++ src/evaluation/example_no_trainer.sh
@@ -0,0 +1,6 @@
+python src/evaluation/example_no_trainer.py \
+    --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
+    --dataset_name random_data.jsonl \
+    --batch_size 16 \
+    --max_new_tokens 100 \
+    --temperature 0.1 \
