e9fa1e321333a14d9d1afee83b0fe7f7bbce962c
ba144220 <62748666+ba144220@users.noreply.github.com>
2024-10-19 02:22:09 -0700

    Revert "Chore/read dataset"

--- src/evaluation/README.md
+++
@@ -1,8 +0,0 @@
-# Evaluation
-
-## How to run evaluation examples
-1. Change the config in the `example_no_trainer.sh` file.
-2. Run:
-```bash
-bash src/evaluation/example_no_trainer.sh
-```
--- src/evaluation/example_no_trainer.sh
+++
@@ -1,8 +0,0 @@
-python src/evaluation/example_no_trainer.py \
-    --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
-    --dataset_name self_generated \
-    --dataset_split_type test \
-    --table_ext .csv \
-    --batch_size 16 \
-    --max_new_tokens 100 \
-    --temperature 0.1 \
